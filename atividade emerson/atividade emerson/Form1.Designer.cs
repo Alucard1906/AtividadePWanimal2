ad35b425cb29dbfbf61e7ee03e1994a2d6e93add
Dimitri G <141324496+Alucard1906@users.noreply.github.com>
2023-08-17 13:41:12 -0300

    Add files via upload

---
+++ atividade emerson/atividade emerson/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace atividade_emerson
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tx1 = new System.Windows.Forms.TextBox();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pcima = new System.Windows.Forms.PictureBox();
+            this.bt5 = new System.Windows.Forms.Button();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.bt3 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcima)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tx1
+            // 
+            resources.ApplyResources(this.tx1, "tx1");
+            this.tx1.Name = "tx1";
+            this.tx1.TextChanged += new System.EventHandler(this.tx1_TextChanged);
+            // 
+            // bt1
+            // 
+            resources.ApplyResources(this.bt1, "bt1");
+            this.bt1.Name = "bt1";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // lb1
+            // 
+            resources.ApplyResources(this.lb1, "lb1");
+            this.lb1.Name = "lb1";
+            // 
+            // pictureBox5
+            // 
+            resources.ApplyResources(this.pictureBox5, "pictureBox5");
+            this.pictureBox5.Image = global::atividade_emerson.Properties.Resources.gatin;
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.bt2_Click);
+            // 
+            // pictureBox4
+            // 
+            resources.ApplyResources(this.pictureBox4, "pictureBox4");
+            this.pictureBox4.Image = global::atividade_emerson.Properties.Resources.cha;
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.bt3_Click);
+            // 
+            // pictureBox3
+            // 
+            resources.ApplyResources(this.pictureBox3, "pictureBox3");
+            this.pictureBox3.Image = global::atividade_emerson.Properties.Resources.tri;
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.bt5_Click);
+            // 
+            // pictureBox2
+            // 
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Image = global::atividade_emerson.Properties.Resources.negao;
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.bt4_Click);
+            // 
+            // pcima
+            // 
+            resources.ApplyResources(this.pcima, "pcima");
+            this.pcima.BackgroundImage = global::atividade_emerson.Properties.Resources.cha;
+            this.pcima.Image = global::atividade_emerson.Properties.Resources.gatin;
+            this.pcima.Name = "pcima";
+            this.pcima.TabStop = false;
+            // 
+            // bt5
+            // 
+            resources.ApplyResources(this.bt5, "bt5");
+            this.bt5.BackgroundImage = global::atividade_emerson.Properties.Resources.tri;
+            this.bt5.Name = "bt5";
+            this.bt5.UseVisualStyleBackColor = true;
+            this.bt5.Click += new System.EventHandler(this.bt5_Click);
+            this.bt5.MouseHover += new System.EventHandler(this.bt5_MouseHover);
+            // 
+            // bt4
+            // 
+            resources.ApplyResources(this.bt4, "bt4");
+            this.bt4.BackgroundImage = global::atividade_emerson.Properties.Resources.negao;
+            this.bt4.Name = "bt4";
+            this.bt4.UseVisualStyleBackColor = true;
+            this.bt4.Click += new System.EventHandler(this.bt4_Click);
+            this.bt4.MouseHover += new System.EventHandler(this.bt4_MouseHover);
+            // 
+            // bt3
+            // 
+            resources.ApplyResources(this.bt3, "bt3");
+            this.bt3.BackgroundImage = global::atividade_emerson.Properties.Resources.cha;
+            this.bt3.Name = "bt3";
+            this.bt3.UseVisualStyleBackColor = true;
+            this.bt3.Click += new System.EventHandler(this.bt3_Click);
+            this.bt3.MouseHover += new System.EventHandler(this.bt3_MouseHover);
+            // 
+            // bt2
+            // 
+            resources.ApplyResources(this.bt2, "bt2");
+            this.bt2.BackgroundImage = global::atividade_emerson.Properties.Resources.gatin;
+            this.bt2.Name = "bt2";
+            this.bt2.UseVisualStyleBackColor = true;
+            this.bt2.Click += new System.EventHandler(this.bt2_Click);
+            this.bt2.MouseHover += new System.EventHandler(this.bt2_MouseHover);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pcima);
+            this.Controls.Add(this.lb1);
+            this.Controls.Add(this.bt5);
+            this.Controls.Add(this.bt4);
+            this.Controls.Add(this.bt3);
+            this.Controls.Add(this.bt2);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.tx1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcima)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tx1;
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.PictureBox pcima;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ atividade emerson/atividade emerson/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace atividade_emerson
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt2_Click(object sender, EventArgs e)
+        {
+            pcima.Image = Properties.Resources.gatin;
+        }
+
+        private void bt4_Click(object sender, EventArgs e)
+        {
+            pcima.Image = Properties.Resources.negao;
+        }
+
+        private void bt3_Click(object sender, EventArgs e)
+        {
+            pcima.Image = Properties.Resources.cha;
+        }
+
+        private void bt5_Click(object sender, EventArgs e)
+        {
+            pcima.Image = Properties.Resources.tri;
+        }
+
+        private void bt2_MouseHover(object sender, EventArgs e)
+        {
+            pcima.Image = Properties.Resources.gatin;
+        }
+
+        private void bt3_MouseHover(object sender, EventArgs e)
+        {
+            pcima.Image = Properties.Resources.cha;
+        }
+
+        private void bt4_MouseHover(object sender, EventArgs e)
+        {
+            pcima.Image = Properties.Resources.negao;
+        }
+
+        private void bt5_MouseHover(object sender, EventArgs e)
+        {
+            pcima.Image = Properties.Resources.tri;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //botão "Não Há Animal"
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            string nome = tx1.Text ;
+
+            MessageBox.Show("Animal Digitado é:" + nome, "\nNão ha Aimais!!!!");
+        }
+       
+        private void tx1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
